64dcc74bd7a01e4519fff703716e10d2cdb41c3a
Romina Verza <131399903+aldebar9n@users.noreply.github.com>
2023-04-25 23:50:36 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+let intentos = 6;
+let palabra = "CLOUD";
+
+window.addEventListener('load', init)
+function init(){
+    console.log('Esto se ejecuta solo cuando se carga la pagina web')
+}
+
+const BOTON = document.getElementById("guess-button");
+BOTON.addEventListener('click' , intentar);
+   
+
+function leerIntento(){
+    let intento = document.getElementById("guess-input");
+    intento = intento.value;
+    intento = intento.toUpperCase(); 
+    return intento;
+ }
+
+ function intentar(){
+    const INTENTO = leerIntento();
+    if (INTENTO === palabra ) {
+        terminar("<h1>GANASTE!😀</h1>")
+        return
+    }
+    const GRID = document.getElementById("grid")
+    const ROW = document.createElement ('div');
+    ROW.className = 'row';
+    for (let i in palabra){
+        const SPAN = document.createElement('span');
+        SPAN.className = 'letter'
+        SPAN.innerHTML = INTENTO [i];
+        if (INTENTO [i] ===palabra [i]){
+            // RED;
+            SPAN.style.backgroundColor = 'red' 
+        } else if( palabra.includes(INTENTO[i]) ) {
+            // ORANGE
+            SPAN.style.backgroundColor = 'orange'
+        } else {
+           // GREY
+           SPAN.style.backgroundColor = 'grey'
+        }
+        ROW.appendChild(SPAN);
+    }
+    GRID.appendChild (ROW)
+		intentos--
+    if (intentos==0){
+        terminar("<h1>PERDISTE!😖</h1>")
+    }
+}
+
+function terminar(mensaje){
+    const INPUT = document.getElementById("guess-input");
+    INPUT.disabled = true;
+    BOTON.disabled = true;
+    let contenedor = document.getElementById('guesses');
+    contenedor.innerHTML = mensaje;
+}
+
